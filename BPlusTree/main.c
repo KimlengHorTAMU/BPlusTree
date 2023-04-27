63b9d5749f161da86c8fb1fd2c59845bbfc18869
Kimleng <kimlenghor2006@gmail.com>
2023-04-27 12:43:34 -0500

    fix memory allocation

--- BPlusTree/main.c
+++ BPlusTree/main.c
@@ -7,62 +7,87 @@
 #include "treemain.h"
 
 int main(int argc, char* arv[]) {
-    
-    //sparse
-//    setupSparseTree(6);
-    
-    //dense
-    setupDenseTree(4);
+
+    //generate the data
+    generateData();
     
     //create a root and initialize to null
     tree_node *bTree = NULL;
-
-    //insert into the tree
-    bTree = insertion(bTree, 7);
-    bTree = insertion(bTree, 10);
-    bTree = insertion(bTree, 1);
-    bTree = insertion(bTree, 23);
-    bTree = insertion(bTree, 5);
-    bTree = insertion(bTree, 15);
-    bTree = insertion(bTree, 17);
-    bTree = insertion(bTree, 9);
-    bTree = insertion(bTree, 11);
-    bTree = insertion(bTree, 39);
-    bTree = insertion(bTree, 35);
-    bTree = insertion(bTree, 8);
-    bTree = insertion(bTree, 40);
-    bTree = insertion(bTree, 25);
     
-//    //generate the data
-//    generateData();
+    //sparse
+    setupSparseTree(13);
+    
+    //insert into b+tree
+    for (int i = 0; i < NUM_SEARCH_KEYS; i++) {
+        bTree = insertion(bTree, search_keys[i]);
+    }
+    showBTree(bTree);
+    
+//    bTree = NULL;
+//    setupSparseTree(24);
 //
 //    //insert into b+tree
 //    for (int i = 0; i < NUM_SEARCH_KEYS; i++) {
 //        bTree = insertion(bTree, search_keys[i]);
 //    }
-    
-    //show the tree in the console
-    showBTree(bTree);
-    
-    //search
-    search(bTree, 35, true);
-    search(bTree, 1, true);
-    search(bTree, 2, true);
-    search(bTree, 5, true);
-    search(bTree, 98, true);
-    search(bTree, 99, true);
-    
-    rangeSearch(bTree, 1, 99);
-    
-//    delete nodes
-    bTree = deletion(bTree, 1);
-    bTree = deletion(bTree, 7);
-    bTree = deletion(bTree, 11);
-    bTree = deletion(bTree, 96);
-    bTree = deletion(bTree, 40);
-    showBTree(bTree);
-    
-    printf("\n");
+//    showBTree(bTree);
+//
+//    //dense
+//    bTree = NULL;
+//    setupDenseTree(13);
+//
+//    //insert into b+tree
+//    for (int i = 0; i < NUM_SEARCH_KEYS; i++) {
+//        bTree = insertion(bTree, search_keys[i]);
+//    }
+//    showBTree(bTree);
+//
+//    bTree = NULL;
+//    setupDenseTree(24);
+//
+//    //insert into b+tree
+//    for (int i = 0; i < NUM_SEARCH_KEYS; i++) {
+//        bTree = insertion(bTree, search_keys[i]);
+//    }
+//    showBTree(bTree);
+
+//    bTree = NULL;
+//    setupDenseTree(4);
+////    insert into the tree
+//    bTree = insertion(bTree, 7);
+//    bTree = insertion(bTree, 10);
+//    bTree = insertion(bTree, 1);
+//    bTree = insertion(bTree, 23);
+//    bTree = insertion(bTree, 5);
+//    bTree = insertion(bTree, 15);
+//    bTree = insertion(bTree, 17);
+//    bTree = insertion(bTree, 9);
+//    bTree = insertion(bTree, 11);
+//    bTree = insertion(bTree, 39);
+//    bTree = insertion(bTree, 35);
+//    bTree = insertion(bTree, 8);
+//    bTree = insertion(bTree, 40);
+//    bTree = insertion(bTree, 25);
+//
+//    //show the tree in the console
+//    showBTree(bTree);
+//
+//    //search
+//    search(bTree, 35, true);
+//    search(bTree, 1, true);
+//    search(bTree, 2, true);
+//    search(bTree, 5, true);
+//    search(bTree, 98, true);
+//    search(bTree, 99, true);
+//
+//    rangeSearch(bTree, 1, 99);
+//
+//    //delete nodes
+//    bTree = deletion(bTree, 1);
+//    bTree = deletion(bTree, 7);
+//    bTree = deletion(bTree, 11);
+//    bTree = deletion(bTree, 96);
+//    bTree = deletion(bTree, 40);
+//
+//    showBTree(bTree);
 }
-
-
--- BPlusTree/treemain.h
+++ BPlusTree/treemain.h
@@ -5,8 +5,8 @@
 //  Created by Kimleng Hor on 4/16/23.
 //
 
-#ifndef treemain_h
-#define treemain_h
+#ifndef tree_h
+#define tree_h
 
 #include <stdlib.h>
 #include <string.h>
@@ -27,27 +27,9 @@ typedef struct tree_node {
     struct tree_node *parent;
 } tree_node;
 
-int search_keys[NUM_SEARCH_KEYS];
+int *search_keys;
 tree_node *linked_list = NULL;
 int tree_order;
-
-//for generating the data
-void generateData(void);
-int contains(int* arr, int size, int value);
-
-void setupSparseTree(int number);
-void setupDenseTree(int number);
-
-void addToTail(tree_node *new_node);
-void showBTree(tree_node *const root);
-
-tree_node *search(tree_node *const tree, int key, bool isSearching);
-void rangeSearch(tree_node *const tree, int begin, int end);
-
-tree_node *addToParent(tree_node *tree, int key, tree_node *left, tree_node *right);
-tree_node *insertion(tree_node *tree, int key);
-
-tree_node *deletion(tree_node *root, int key);
 
 int contains(int* arr, int size, int value) {
     for (int i = 0; i < size; i++) {
@@ -58,7 +40,10 @@ int contains(int* arr, int size, int value) {
     return 0;
 }
 
-void generateData() {
+void generateData(void) {
+    
+    search_keys = (int*) malloc(NUM_SEARCH_KEYS * sizeof(int));
+    
     int num_keys = 0;
     while (num_keys < NUM_SEARCH_KEYS) {
         int randomKey = rand() % (MAX_SEARCH_KEY - MIN_SEARCH_KEY + 1) + MIN_SEARCH_KEY;
@@ -77,66 +62,121 @@ void setupDenseTree(int number) {
     tree_order = number + 1;
 }
 
-//add the new node into the linked list
-void addToTail(tree_node *new_node) {
-    tree_node *tree;
-    if (linked_list != NULL) {
-        tree = linked_list;
-        for (; tree->next != NULL; tree = tree->next) {}
-        tree->next = new_node;
-        new_node->next = NULL;
-    } else {
-        linked_list = new_node;
-    }
-}
-
-//show tree in the console
-void showBTree(tree_node *tree) {
-    int level = 0;
-    int next_level = 0;
-    tree_node *node = NULL;
-
-    linked_list = NULL;
-    addToTail(tree);
+tree_node *addToParent(tree_node *tree, int key, tree_node *left_node, tree_node *right_node) {
+    int index = 0;
+    int split_point = (int) ceil((double) tree_order / 2);
+    int split_key;
     
-    printf("--> ");
-    while (linked_list != NULL) {
-        next_level = 0;
-        
-        //remove from the head
-        tree_node *temp_node = linked_list;
-        linked_list = linked_list->next;
-        node = temp_node;
-        
-        if (node->parent != NULL) {
-            tree_node *child = node;
-            while (child != tree) {
-                child = child->parent;
-                next_level++;
+    tree_node *new_tree;
+    tree_node *child;
+    
+    int *parent_keys;
+    tree_node **parent_pointers;
+    tree_node *parent;
+    
+    int i = 0;
+    int j = 0;
+    
+    parent_pointers = malloc((tree_order + 1) * sizeof(tree_node *));
+    parent_keys = malloc(tree_order * sizeof(int));
+
+    parent = left_node->parent;
+
+    if (parent == NULL) {
+        new_tree = malloc(sizeof(tree_node));
+        new_tree->keys = malloc((tree_order - 1) * sizeof(int));
+        new_tree->p = malloc(tree_order * sizeof(void *));
+        new_tree->leaf_node = false;
+        new_tree->keys[0] = key;
+        new_tree->p[0] = left_node;
+        new_tree->p[1] = right_node;
+        new_tree->length++;
+        new_tree->parent = NULL;
+        left_node->parent = new_tree;
+        right_node->parent = new_tree;
+        return new_tree;
+    }
+
+    for (; index <= parent->length && parent->p[index] != left_node; index++) {}
+
+    if (parent->length >= tree_order - 1) {
+        while (i < parent->length + 1) {
+            if (j == index + 1) {
+                j++;
             }
-            
-            if (next_level != level) {
-                level = next_level;
-                printf("\n--> ");
-            }
+            parent_pointers[j] = parent->p[i];
+            i++;
+            j++;
         }
+
+        i = 0;
+        j = 0;
+        while (i < parent->length) {
+            if (j == index) {
+                j++;
+            }
+            parent_keys[j] = parent->keys[i];
+            i++;
+            j++;
+        }
+
+        parent_pointers[index + 1] = right_node;
+        parent_keys[index] = key;
         
-        int i = 0;
-        printf("[ ");
-        while (i < node->length) {
-            printf("%d ", node->keys[i]);
+        new_tree = malloc(sizeof(tree_node));
+        new_tree->keys = malloc((tree_order - 1) * sizeof(int));
+        new_tree->p = malloc(tree_order * sizeof(void *));
+        new_tree->leaf_node = false;
+        new_tree->length = 0;
+        new_tree->parent = NULL;
+        new_tree->next = NULL;
+        
+        parent->length = 0;
+        
+        i = 0;
+        while (i < split_point - 1) {
+            parent->p[i] = parent_pointers[i];
+            parent->keys[i] = parent_keys[i];
+            parent->length++;
             i++;
         }
         
-        if (node->leaf_node == false) {
-            int j = 0;
-            while (j <= node->length) {
-                addToTail(node->p[j]);
-                j++;
-            }
+        parent->p[i] = parent_pointers[i];
+        split_key = parent_keys[split_point - 1];
+        
+        j = 0;
+        i++;
+        while (i < tree_order) {
+            new_tree->p[j] = parent_pointers[i];
+            new_tree->keys[j] = parent_keys[i];
+            new_tree->length++;
+            i++;
+            j++;
         }
-
-        printf("] ");
+        
+        new_tree->p[j] = parent_pointers[i];
+        new_tree->parent = parent->parent;
+        
+        i = 0;
+        while (i <= new_tree->length) {
+            child = new_tree->p[i];
+            child->parent = new_tree;
+            i++;
+        }
+        
+        return addToParent(tree, split_key, parent, new_tree);
+    } else {
+        int i = parent->length;
+        while (i > index) {
+            parent->p[i + 1] = parent->p[i];
+            parent->keys[i] = parent->keys[i - 1];
+            i--;
+        }
+        parent->p[index + 1] = right_node;
+        parent->keys[index] = key;
+        parent->length++;
+        
+        return tree;
     }
 }
 
@@ -165,130 +205,6 @@ tree_node *search(tree_node *const tree, int key, bool isSearching) {
     }
     
     return node;
-}
-
-void rangeSearch(tree_node *const tree, int begin, int end) {
-    int numbers = 0;
-    int keys_found[end - begin + 1];
-    int order = tree_order - 1;
-    
-    tree_node *node = search(tree, begin, false);
-  
-    while (node) {
-        for (int i = 0; i < node->length; i++) {
-            if (node->keys[i] >= begin && node->keys[i] <= end) {
-                keys_found[numbers++] = node->keys[i];
-            }
-        }
-        node = node->p[order];
-    }
-    
-    printf("\nKeys that are found in this range: ");
-    for (int i = 0; i < numbers; i++) {
-        printf("%d, ", keys_found[i]);
-    }
-    printf("\n");
-}
-
-tree_node *addToParent(tree_node *tree, int key, tree_node *left_node, tree_node *right_node) {
-    tree_node *new_tree;
-    new_tree = malloc(sizeof(tree_node));
-    new_tree->p = malloc(tree_order * sizeof(void *));
-    new_tree->keys = malloc((tree_order - 1) * sizeof(int));
-    
-    tree_node *parent = left_node->parent;
-    int index = 0;
-    
-    int split_point = (int) ceil((double) tree_order / 2);
-    int split_key;
-    int *parent_keys;
-    tree_node *child;
-    tree_node **parent_pointers;
-    int i = 0;
-    int j = 0;
-    
-    parent_pointers = malloc((tree_order + 1) * sizeof(tree_node *));
-    parent_keys = malloc(tree_order * sizeof(int));
-
-    if (parent == NULL) {
-        new_tree->length++;
-        new_tree->keys[0] = key;
-        new_tree->p[1] = right_node;
-        new_tree->p[0] = left_node;
-        left_node->parent = new_tree;
-        right_node->parent = new_tree;
-        return new_tree;
-    }
-    
-    for (; index <= parent->length && parent->p[index] != left_node; index++) {}
-    
-    if (parent->length >= tree_order - 1) {
-        while (i < parent->length + 1) {
-            if (j == index + 1) {
-                j++;
-            }
-            parent_pointers[j] = parent->p[i];
-            i++;
-            j++;
-        }
-        
-        i = 0;
-        j = 0;
-        while (i < parent->length) {
-            if (j == index) {
-                j++;
-            }
-            parent_keys[j] = parent->keys[i];
-            i++;
-            j++;
-        }
-
-        parent_pointers[index + 1] = right_node;
-        parent_keys[index] = key;
-        parent->length = 0;
-        split_key = parent_keys[split_point - 1];
-       
-        i = 0;
-        while (i < split_point - 1) {
-            parent->p[i] = parent_pointers[i];
-            parent->keys[i] = parent_keys[i];
-            parent->length++;
-            i++;
-        }
-
-        j = 0;
-        i++;
-        while (i < tree_order) {
-            new_tree->p[j] = parent_pointers[i];
-            new_tree->keys[j] = parent_keys[i];
-            new_tree->length++;
-            i++;
-            j++;
-        }
-        
-        new_tree->p[j] = parent_pointers[i];
-        
-        i = 0;
-        while (i < new_tree->length) {
-            child = new_tree->p[i];
-            child->parent = new_tree;
-            i++;
-        }
-
-        return addToParent(tree, split_key, parent, new_tree);
-    } else {
-        int i = parent->length;
-        while (i > index) {
-            parent->p[i + 1] = parent->p[i];
-            parent->keys[i] = parent->keys[i - 1];
-            i--;
-        }
-        parent->p[index + 1] = right_node;
-        parent->keys[index] = key;
-        parent->length++;
-        
-        return tree;
-    }
 }
 
 tree_node *insertion(tree_node *tree, int key) {
@@ -379,9 +295,32 @@ tree_node *insertion(tree_node *tree, int key) {
     }
 }
 
-tree_node *deletion(tree_node *root, int key) {
+void rangeSearch(tree_node *const tree, int begin, int end) {
+    int numbers = 0;
+    int keys_found[end - begin + 1];
+    int order = tree_order - 1;
+    
+    tree_node *node = search(tree, begin, false);
+  
+    while (node) {
+        for (int i = 0; i < node->length; i++) {
+            if (node->keys[i] >= begin && node->keys[i] <= end) {
+                keys_found[numbers++] = node->keys[i];
+            }
+        }
+        node = node->p[order];
+    }
+    
+    printf("\nKeys that are found in this range: ");
+    for (int i = 0; i < numbers; i++) {
+        printf("%d, ", keys_found[i]);
+    }
+    printf("\n");
+}
+
+tree_node *deletion(tree_node *tree, int key) {
     tree_node *leaf = NULL;
-    leaf = search(root, key, false);
+    leaf = search(tree, key, false);
     for (int i = 0; i < leaf->length; i++) {
         if (leaf->keys[i] == key) {
             leaf->keys[i] = leaf->keys[i + 1];
@@ -390,9 +329,70 @@ tree_node *deletion(tree_node *root, int key) {
         }
     }
     
-    return root;
+    return tree;
 }
 
-#endif /* treemain_h */
+//add the new node into the linked list
+void addToTail(tree_node *new_node) {
+    tree_node *tree;
+    if (linked_list != NULL) {
+        tree = linked_list;
+        for (; tree->next != NULL; tree = tree->next) {}
+        tree->next = new_node;
+        new_node->next = NULL;
+    } else {
+        linked_list = new_node;
+    }
+}
 
+//show tree in the console
+void showBTree(tree_node *tree) {
+    int level = 0;
+    int next_level = 0;
+    tree_node *node = NULL;
 
+    linked_list = NULL;
+    addToTail(tree);
+    
+    printf("--> ");
+    while (linked_list != NULL) {
+        next_level = 0;
+        
+        //remove from the head
+        tree_node *temp_node = linked_list;
+        linked_list = linked_list->next;
+        node = temp_node;
+        
+        if (node->parent != NULL) {
+            tree_node *child = node;
+            while (child != tree) {
+                child = child->parent;
+                next_level++;
+            }
+            
+            if (next_level != level) {
+                level = next_level;
+                printf("\n--> ");
+            }
+        }
+        
+        int i = 0;
+        printf("[ ");
+        while (i < node->length) {
+            printf("%d ", node->keys[i]);
+            i++;
+        }
+        
+        if (node->leaf_node == false) {
+            int j = 0;
+            while (j <= node->length) {
+                addToTail(node->p[j]);
+                j++;
+            }
+        }
+
+        printf("] ");
+    }
+}
+
+#endif /* tree_h */
